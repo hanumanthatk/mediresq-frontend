2e7eb7fb32f2671df26eaae55c46858e487dd6f9
hanumanthatk <hanumanthatkgowda@gmail.com>
2026-05-16 15:23:52 +0530

    fixed frontend api paths

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -1,9 +1,17 @@
 import api from './axios'
 
 export const authApi = {
-  register: (data) => api.post('/auth/register', data),
-  login:    (data) => api.post('/auth/login', data),
-  refresh:  (token) => api.post('/auth/refresh', null, {
-    headers: { 'Refresh-Token': token }
-  }),
+
+  register: (data) =>
+    api.post('/api/auth/register', data),
+
+  login: (data) =>
+    api.post('/api/auth/login', data),
+
+  refresh: (token) =>
+    api.post('/api/auth/refresh', null, {
+      headers: {
+        'Refresh-Token': token
+      }
+    }),
 }
